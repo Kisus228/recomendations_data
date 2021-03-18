b7ed565906a2a6b435756edb7eb4b05cfef0ea1e
sukhr <sukhrab2001@mail.ru>
2021-03-18 22:55:51 +0500

    Сортировка листа словарей по INN, чтобы нагляднее видеть деиствия пользователя. Вывод по одному словарю в строку

--- request.py
+++ request.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import operator
 
 headers = {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"}
 
@@ -9,6 +11,12 @@ dates = {'start': '01.01.2018', 'finish': '01.02.2018'}
 
 urlData = requests.post(url, json=dates, headers=headers)
 
-print(urlData.json())
+# with open(urlData.json()) as json_file:
+#     data = json.load(json_file)
+parsed = json.loads(urlData.json())
+parsed.sort(key=operator.itemgetter('INN'))
+for x in(parsed):
+    print(x)
+#print(urlData.json())
 
 print(urlData.status_code)
