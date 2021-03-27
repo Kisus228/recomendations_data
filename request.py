73c20a6e4c0f1d315175e2e5c3862bb25bfb393f
sukhr <sukhrab2001@mail.ru>
2021-03-27 23:09:37 +0500

    разбил предыдущие изменения на отдельные функции.

--- request.py
+++ request.py
@@ -1,21 +1,28 @@
 import requests
 import json
-import operator
+import operator as op
+
+
+def createUrlData(url, dates, headers):
+    return requests.post(url, json=dates, headers=headers)
+
+
+def sortAndPrint(data, key):
+    parsed = json.loads(data.json())
+    parsed.sort(key=op.itemgetter('INN'))
+    for x in parsed:
+        print(x)
+
 
 headers = {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"}
-
 
 url = 'http://localhost:8080/execmodel'
 
 dates = {'start': '01.01.2018', 'finish': '01.02.2018'}
 
-urlData = requests.post(url, json=dates, headers=headers)
+urlData = createUrlData('http://localhost:8080/execmodel', {'start': '01.01.2018', 'finish': '01.02.2018'},
+                        {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"})
 
-
-parsed = json.loads(urlData.json())
-parsed.sort(key=operator.itemgetter('INN'))
-for x in parsed:
-    print(x)
-
+sortAndPrint(urlData, 'INN')
 
 print(urlData.status_code)
