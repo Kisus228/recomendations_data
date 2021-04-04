3244a4be79732fc45c6de125841cd1df83b417a8
andrewzvorygin <Andrei.Zvorygin@urfu.me >
2021-04-04 14:28:21 +0500

    key в методе sort and print

--- request.py
+++ request.py
@@ -9,7 +9,7 @@ def createUrlData(url, dates, headers):
 
 def sortAndPrint(data, key):
     parsed = json.loads(data.json())
-    parsed.sort(key=op.itemgetter('INN'))
+    parsed.sort(key=op.itemgetter(key))
     for x in parsed:
         print(x)
 
@@ -20,8 +20,7 @@ url = 'http://localhost:8080/execmodel'
 
 dates = {'start': '01.01.2018', 'finish': '01.02.2018'}
 
-urlData = createUrlData('http://localhost:8080/execmodel', {'start': '01.01.2018', 'finish': '01.02.2018'},
-                        {"Content-Type": "application/json", "X-Auth-Token": "4CE7B412-49B7-3DCF-B56D-3441B6A3698A"})
+urlData = createUrlData(url, dates, headers)
 
 sortAndPrint(urlData, 'INN')
 
